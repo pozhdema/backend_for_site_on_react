0ce147c2b368889425ef3fcbdd7fd47bcf4d9b70
Pozhdema <pozhdema107@gmail.com>
2020-06-11 11:40:25 +0300

    fixed application

--- core/Application.php
+++ core/Application.php
@@ -15,8 +15,14 @@ class Application
         define("CORE_PATH", dirname(__FILE__) . "/");
         define("BASE_PATH", dirname(__FILE__, 2) . "/");
         spl_autoload_register(function ($className) {
-            $className = explode("\\", strtolower($className));
-            $className[count($className) - 1] = ucfirst($className[count($className) - 1]) . ".php";
+            $className = explode("\\", $className);
+            foreach ($className as $index => $word) {
+                if ($index == count($className)-1) {
+                    $className[$index] = $word . ".php";
+                } else {
+                    $className[$index] = strtolower($word);
+                }
+            }
             $className = BASE_PATH . implode("/", $className);
             if (file_exists($className)) {
                 require_once($className);
@@ -40,14 +46,14 @@ class Application
     {
         $session = new Session();
         ACL::getInstance()->setRoleID($session->getSession("role_id"));
-        if (ACL::getInstance()->isAllowed($router->getController(), $router->getAction())){
+        if (ACL::getInstance()->isAllowed($router->getController(), $router->getAction())) {
             /** @var Controller $controller */
             $controller->setRequest(new Request());
             $controller->setResponse(new Response());
             $controller->setSession($session);
             $controller->setConfig(yaml_parse_file(BASE_PATH . "config.yaml"));
             $controller->$actionName();
-        }else{
+        } else {
             http_response_code(401);
             exit("Access denied");
         }
